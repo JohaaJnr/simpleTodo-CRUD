f379963cb141c53e8d897b3fa42809e1f0d12dc1
JohaaJnr <avoid.zoha@gmail.com>
2021-07-01 05:53:29 +0600

    jquerytodo simple

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     
 var count = 1;
+$('#todos').hide();
 
 $('#form').on('submit', function(e){
     e.preventDefault();
     let addtask = $('#textarea').val();
-
-   
-   let counts = count++;
+    
+    let counts = count++;
   
+    $('#todos').show();
     $('#tasks').append(`
     <tr id="${counts}">
     <th ><i class="bi bi-arrow-return-right"></i></th>
@@ -16,6 +17,9 @@ $('#form').on('submit', function(e){
     <td ><button class="btnn" onclick="$('#${counts}').remove();"><i class="bi bi-trash"></i></button></td>
     </tr>
     `);
+    
     $('#textarea').val('');
+    
 });
 });
+
